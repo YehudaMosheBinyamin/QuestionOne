a554fdd7bd9c3e4a5d170281d82fc5660cf6248d
yehudaben2@gmail.com <yehudaben2@gmail.com>
2021-03-07 19:02:25 +0200

    commit of test

--- QuestionOneFixed/main.c
+++ QuestionOneFixed/main.c
@@ -8,9 +8,9 @@ int main(void)
     assert(ChangeContents("C:\\NonExistingFolder\\file.txt") == FAILED_OPERATION);
     assert(ChangeContents("C:\\Windows\\nothepad.exe") == FAILED_OPERATION);
     assert(ChangeContents("C:\\Users\\danbe\\Desktop\\NonExistantDocument.txt") == FAILED_OPERATION);
-    //Important note: if the computer's security settings\antivirus settings  allow changing the contents of files in Desktop,
-    // the folowing test will fail and the function will return SUCCESSFUL_OPERATION 
-    assert(ChangeContents("C:\\Users\\danbe\\Desktop\\NewDocument.txt") == FAILED_OPERATION);
+    // Note: if computer's security settings/antivirus settings do not allow changing the contents of files in Desktop,
+    // the folowing test will fail and the function will return FAILED_OPERATION 
+    assert(ChangeContents("C:\\Users\\danbe\\Desktop\\NewDocument.txt") == SUCCESSFUL_OPERATION);
     assert(ChangeContents("C:\\Windows\\nonexistantfile") == FAILED_OPERATION);
     // This case test be proved by checking the text file
     assert(ChangeContents("\\\\localhost\\C$\\NewFolder\\TextDocument.txt") == SUCCESSFUL_OPERATION);
